ebfb2a4195dcd8446c36ebba227d7165e16fb29a
Roman Kavisnkyi <roman_kavinskyi@pecodesoftware.com>
2022-07-28 12:27:19 +0700

    Initial Commit

---
+++ GitHubRepositories/GitHubRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesApp.swift
+//  GitHubRepositories
+//
+//  Created by Roman Kavinskyi on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
