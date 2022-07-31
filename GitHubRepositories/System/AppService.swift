d763a23ce824724cd52b8db6b54411117a90f1b1
Roman Kavisnkyi <roman_kavinskyi@pecodesoftware.com>
2022-07-31 23:25:02 +0700

    cleanup

--- GitHubRepositories/System/AppService.swift
+++ GitHubRepositories/System/AppService.swift
@@ -35,15 +35,6 @@ extension AppService {
             if showLoader { appState.isLoading = true }
             let results = await clientResult()
             if showLoader { appState.isLoading = false }
-
-//                if case let .failure(error) = result,
-//                   let apiError = error as? APIError,
-//                   case let .unauthorized(message) = apiError,
-//                   message == BackendErrors.unauthenticated {
-//                    appState.logout()
-//                    return
-//                }
-            
             
             results.forEach {
                 resultHandler($0)
