079b0f0d2b04c6f3f7cce63b2708aeead0eddbdd
Roman Kavisnkyi <roman_kavinskyi@pecodesoftware.com>
2022-07-31 23:17:53 +0700

    Completed project

---
+++ GitHubRepositories/Common/Extensions/Color + ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color + ext.swift
+//  GitHubRepositories
+//
+//  Created by Roman Kavinskyi on 7/30/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static var systemGray: Color {
+        Color(uiColor: .systemGray)
+    }
+    
+    static var buttonBackground: Color {
+        Color(uiColor: UIColor(red: 23/255, green: 21/255, blue: 22/255, alpha: 1))
+    }
+}
